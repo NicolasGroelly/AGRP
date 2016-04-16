60adf321ae66ca8dd10333a4feece5e927c6821d
Nicolas Groelly <nicolas.groelly@me.com>
2016-04-16 19:07:39 +0200

    Alpha

--- history.php
+++ history.php
@@ -26,8 +26,8 @@ require_once "Require/header.php";
         <tbody>
         <?php foreach ($history->getAll() as $k => $v) : ?>
             <tr class="<?php if ($v["status"] == 1) { echo "success"; } else { echo "danger"; } ?>">
-                <td><?php echo date("d/m/Y", $v["timestamp"]); ?></td>
-                <td><?php echo date("H:i", $v["timestamp"]); ?></td>
+                <td><?php $d = date_parse($v["timestamp"]); echo $d["day"] . "/" . $d["month"] . "/" . $d["year"]; ?></td>
+                <td><?php echo $d["hour"] . ":" . $d["minute"]; ?></td>
                 <td><?php echo $v["plate_0"]; ?></td>
                 <td><?php if ($v["status"] == 1) : ?>Allow<?php else : ?>Deny<?php endif; ?></td>
             </tr>
