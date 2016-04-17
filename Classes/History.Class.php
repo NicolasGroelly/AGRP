5b12e522bb48deab588314a4e6dd1bae1588fbe4
Nicolas Groelly <nicolas.groelly@me.com>
2016-04-17 17:40:19 +0200

    Alpha

--- Classes/History.Class.php
+++ Classes/History.Class.php
@@ -11,4 +11,25 @@ class History extends PdoManager
 
         return $req->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function add($datas, $allow)
+    {
+        $req = $this->pdo->prepare("
+          INSERT INTO history (plate, plate_0, plate_1, plate_2, plate_3, plate_4, plate_5, plate_6, plate_7, plate_8, plate_9, status, image)
+          VALUES (:plate, :plate_0, :plate_1, :plate_2, :plate_3, :plate_4, :plate_5, :plate_6, :plate_7, :plate_8, :plate_9, :status, :image)";
+        $req->bindValue("plate", $datas["results"]["0"]["plate"]);
+        $req->bindValue("plate_0", $datas["results"]["0"]["candidates"]["0"]["plate"]);
+        $req->bindValue("plate_1", $datas["results"]["0"]["candidates"]["1"]["plate"]);
+        $req->bindValue("plate_2", $datas["results"]["0"]["candidates"]["2"]["plate"]);
+        $req->bindValue("plate_3", $datas["results"]["0"]["candidates"]["3"]["plate"]);
+        $req->bindValue("plate_4", $datas["results"]["0"]["candidates"]["4"]["plate"]);
+        $req->bindValue("plate_5", $datas["results"]["0"]["candidates"]["5"]["plate"]);
+        $req->bindValue("plate_6", $datas["results"]["0"]["candidates"]["6"]["plate"]);
+        $req->bindValue("plate_7", $datas["results"]["0"]["candidates"]["7"]["plate"]);
+        $req->bindValue("plate_8", $datas["results"]["0"]["candidates"]["8"]["plate"]);
+        $req->bindValue("plate_9", $datas["results"]["0"]["candidates"]["9"]["plate"]);
+        $req->bindValue("allow", $allow);
+        $req->bindValue("image", $datas["uuid"] . ".jpg");
+        $req->execute();
+    }
 }
---
+++ api/push/index.php
@@ -0,0 +1,32 @@
+<?php
+$dates = file_get_contents('php://input');
+$datas = json_decode($datas);
+
+$plates = $datas["results"]["0"];
+
+$ip = "10.255.0.2";
+$url = "http://admin:admin@" . $ip . "/status.xml?pl1=1";
+
+$allow = 0;
+
+require_once "../../required.php";
+
+if ($plate->isAllow($plates["plate"])) {
+    $allow = 1;
+} else {
+    foreach ($plates["candidates"] as $k => $v) {
+        if ($plate->isAllow($v["plate"])) {
+            $allow = 1;
+        }
+    }
+}
+
+if ($allow == 1) {
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  curl_exec($curl);
+  curl_close($curl);
+} 
+
+$history->add($datas, $allow);
