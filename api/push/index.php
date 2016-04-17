0d0d61353ad587c2e7fef3b0f1fb3ba2dd1ceec6
Nicolas Groelly <nicolas.groelly@me.com>
2016-04-17 20:36:20 +0200

    Alpha

--- api/push/index.php
+++ api/push/index.php
@@ -5,7 +5,6 @@ $datas = json_decode($datas, true);
 $plates = $datas["results"]["0"];
 
 $ip = "10.255.0.2";
-$url = "http://" . $ip . "/status.xml?pl1=1";
 
 $allow = 0;
 
@@ -24,11 +23,23 @@ foreach ($plates["candidates"] as $k => $v) {
 
 
 if ($allow == 1) {
-  $curl = curl_init();
-  curl_setopt($curl, CURLOPT_URL, $url);
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-  curl_exec($curl);
-  curl_close($curl);
-} 
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, "http://" . $ip . "/status.xml?pl1=1");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_exec($curl);
+    curl_close($curl);
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, "http://" . $ip . "/status.xml?pl2=1");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_exec($curl);
+    curl_close($curl);
+} else {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, "http://" . $ip . "/status.xml?pl3=1");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_exec($curl);
+    curl_close($curl);
+}
 
 $history->add($datas, $allow, $p);
