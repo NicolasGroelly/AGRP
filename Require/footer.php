901a64aaa61964ef7135eb0efe6b4f91d008e28c
Nicolas Groelly <nicolas.groelly@me.com>
2016-04-18 20:01:47 +0200

    Alpha

--- Require/footer.php
+++ Require/footer.php
@@ -1,22 +1,14 @@
 </div>
 
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript" src="/zoombox/zoombox.js"></script>
 
 <script type="text/javascript">
     jQuery(function($){
-         $('a.zoombox').zoombox({
-                theme       : 'lightbox',        //available themes : zoombox,lightbox, prettyphoto, darkprettyphoto, simple
-                opacity     : 0.8,              // Black overlay opacity
-                duration    : 800,              // Animation duration
-                animation   : true,             // Do we have to animate the box ?
-                width       : 600,              // Default width
-                height      : 400,              // Default height
-                gallery     : false,             // Allow gallery thumb view
-                autoplay : false                // Autoplay for video
-            });
-    });
+        $(function(){
+            $('a.zoombox').zoombox();
+        });
 </script>
 </body>
 </html>
